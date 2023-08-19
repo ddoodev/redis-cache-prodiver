aed0bb3c218586060da994a1f6ffd270dec943db
bit0r1n <me@bitor.in>
2023-08-19 16:37:16 +0300

    chore: use redis hash

--- src/RedisCacheProvider.ts
+++ src/RedisCacheProvider.ts
@@ -1,313 +1,106 @@
 import { CacheProvider } from '@discordoo/providers'
 import { AnyDiscordApplication, CompletedCacheOptions } from 'discordoo'
-import { RedisClientType, createClient } from 'redis'
+import { Cluster, ClusterNode, ClusterOptions, Redis, RedisOptions } from 'ioredis'
 
-export enum RedisCacheProviderPerformanceMode {
-  Fast,
-  Saving
+export enum RedisCacheProviderConnectionType {
+  Standalone,
+  Cluster
 }
 
-export type RedisCacheProviderOptions = Parameters<typeof createClient>[0] & {
-  performance?: RedisCacheProviderPerformanceMode
+type RedisClusterOptions = ClusterOptions & {
+  nodes: ClusterNode[]
+}
+
+export type RedisCacheProviderOptions = (RedisOptions | RedisClusterOptions) & {
+  connection?: RedisCacheProviderConnectionType,
+  prefix?: string
 }
 
 export class RedisCacheProvider implements CacheProvider {
-  public readonly redisClient: RedisClientType<any, any, any>
+  public readonly redisClient: Redis | Cluster
 
   public readonly app: AnyDiscordApplication
   public readonly compatible: 'classes' | 'json' | 'text' | 'buffer' = 'text'
   public readonly sharedCache = true
-  public performanceMode: RedisCacheProviderPerformanceMode = RedisCacheProviderPerformanceMode.Fast
+
+  public readonly connectionType: RedisCacheProviderConnectionType = RedisCacheProviderConnectionType.Standalone
+  public readonly prefix?: string
 
   constructor(app: AnyDiscordApplication, cacheOptions: CompletedCacheOptions, providerOptions: RedisCacheProviderOptions) {
-    this.app = app
-    this.redisClient = createClient(providerOptions)
+    this.connectionType = providerOptions.connection ?? RedisCacheProviderConnectionType.Standalone
 
-    if (providerOptions.performance) {
-      this.performanceMode = providerOptions.performance
+    if (providerOptions.prefix) {
+      this.prefix = providerOptions.prefix
     }
+
+    this.app = app
+    this.redisClient = this.connectionType === RedisCacheProviderConnectionType.Standalone ?
+      new Redis({ ...providerOptions, lazyConnect: true }) :
+        new Cluster((providerOptions as RedisClusterOptions).nodes, { ...providerOptions, lazyConnect: true })
   }
 
-  private getRedisKey(keyspace: string, storage?: string, key?: string) {
-    return `${keyspace}:${storage ?? '*'}:${key ?? '*'}`
+  private getStorageKey(keyspace: string, storage: string) {
+    return `${this.prefix ? (this.prefix + ':') : ''}${keyspace}:${storage}`
   }
 
   async get<K = string, V = any>(keyspace: string, storage: string, key: K): Promise<V | undefined> {
-    return (await this.redisClient.get(
-      this.getRedisKey(keyspace, storage, key as string)
+    return (await this.redisClient.hget(
+      this.getStorageKey(keyspace, storage),
+      key as string
     )) as V || undefined
   }
   async set<K = string, V = any>(keyspace: string, storage: string, key: K, value: V): Promise<CacheProvider> {
-    await this.redisClient.set(
-      this.getRedisKey(keyspace, storage, key as string),
+    await this.redisClient.hset(
+      this.getStorageKey(keyspace, storage),
+      key as string,
       value as string
     )
     return this
   }
   async delete<K = string>(keyspace: string, storage: string, key: K | K[]): Promise<boolean> {
-    const keys = (Array.isArray(key) ? key : [ key ]).map(key =>
-      this.getRedisKey(keyspace, storage, key as string))
-    return !!(await this.redisClient.del(keys))
+    const fields = Array.isArray(key) ? key : [ key ]
+    return !!(await this.redisClient.hdel(
+      this.getStorageKey(keyspace, storage),
+      ...fields as string[]
+    ))
   }
   async forEach<K = string, V = any, P extends CacheProvider = CacheProvider>(
     keyspace: string, storage: string, predicate: (value: V, key: K, provider: P) => unknown
   ): Promise<void> {
-    const keys = await this.keys(keyspace, storage)
-
-    if (!keys.length) return
-
-    if (this.performanceMode === RedisCacheProviderPerformanceMode.Fast) {
-      const values = await this.redisClient.mGet(
-        keys.map(key => this.getRedisKey(keyspace, storage, key))
-      )
-  
-      for (let i = 0; i < keys.length; i++) {
-        if (!values[i]) continue
-        await predicate(values[i] as V, keys[i] as K, this as unknown as P)
-      }
-    } else {
-      for (const key of keys) {
-        const value = await this.get(keyspace, storage, key)
-
-        if (value) {
-          await predicate(value as V, key as K, this as unknown as P)
-        }
-      }
+    for (const [ key, value ] of await this.entries(keyspace, storage)) {
+      await predicate(value as V, key as K, this as unknown as P)
     }
   }
   async clear(keyspace: string, storage: string): Promise<boolean> {
-    const keys = await this.keys(keyspace, storage)
-
-    if (!keys.length) return false
-
-    return await this.delete(keyspace, storage, keys)
+    return !!(await this.redisClient.del(
+      this.getStorageKey(keyspace, storage)
+    ))
   }
   async size(keyspace: string, storage: string): Promise<number> {
-    return (await this.keys(keyspace, storage)).length 
+    return await this.redisClient.hlen(
+      this.getStorageKey(keyspace, storage)
+    ) 
   }
   async has<K = string>(keyspace: string, storage: string, key: K): Promise<boolean> {
-    return !!(await this.redisClient.exists(
-      this.getRedisKey(keyspace, storage, key as string)
+    return !!(await this.redisClient.hexists(
+      this.getStorageKey(keyspace, storage),
+      key as string
     ))
-  }
-  async sweep<K = string, V = any, P extends CacheProvider = CacheProvider>(
-    keyspace: string, storage: string, predicate: (value: V, key: K, provider: P) => boolean | Promise<boolean>
-  ): Promise<void> {
-    const keys = await this.keys(keyspace, storage)
-
-    if (!keys.length) return
-
-    const keysToDelete: string[] = []
-
-    if (this.performanceMode === RedisCacheProviderPerformanceMode.Fast) {
-      const values = await this.redisClient.mGet(
-        keys.map(key => this.getRedisKey(keyspace, storage, key))
-      )
-  
-      const keysToDelete: string[] = []
-
-      for (let i = 0; i < keys.length; i++) {
-        if (!values[i]) continue
-        if (await predicate(values[i] as V, keys[i] as K, this as unknown as P)) {
-          keysToDelete.push(keys[i] as string)
-        }
-      }
-    } else {
-      for (const key of keys) {
-        const value = await this.get(keyspace, storage, key)
-
-        if (value && await predicate(value as V, key as K, this as unknown as P)) {
-          keysToDelete.push(key as string)
-        }
-      }
-    }
-
-    if (keysToDelete.length) {
-      await this.delete(keyspace, storage, keysToDelete)
-    }
-  }
-  async filter<K = string, V = any, P extends CacheProvider = CacheProvider>(
-    keyspace: string, storage: string, predicate: (value: V, key: K, provider: P) => boolean | Promise<boolean>
-  ): Promise<[K, V][]> {
-    const keys = await this.keys(keyspace, storage)
-
-    if (!keys.length) return []
-
-    const filtered: [K, V][] = []
-
-    if (this.performanceMode === RedisCacheProviderPerformanceMode.Fast) {
-      const values = await this.redisClient.mGet(
-        keys.map(key => this.getRedisKey(keyspace, storage, key))
-      )
-  
-      for (let i = 0; i < keys.length; i++) {
-        if (!values[i]) continue
-
-        if (await predicate(values[i] as V, keys[i] as K, this as unknown as P)) {
-          filtered.push([ keys[i] as K, values[i] as V ])
-        }
-      }
-    } else {
-      for (const key of keys) {
-        const value = await this.get(keyspace, storage, key)
-
-        if (value && await predicate(value as V, key as K, this as unknown as P)) {
-          filtered.push([ key as K, value as V ])
-        }
-      }
-    }
-
-    return filtered
-  }
-  async map<K = string, V = any, R = any, P extends CacheProvider = CacheProvider>(
-    keyspace: string, storage: string, predicate: (value: V, key: K, provider: P) => R | Promise<R>
-  ): Promise<R[]> {
-    const keys = await this.keys(keyspace, storage)
-
-    if (!keys.length) return []
-
-    const mapped: R[] = []
-
-    if (this.performanceMode === RedisCacheProviderPerformanceMode.Fast) {
-      const values = await this.redisClient.mGet(
-        keys.map(key => this.getRedisKey(keyspace, storage, key))
-      )
-
-      for (let i = 0; i < keys.length; i++) {
-        if (!values[i]) continue
-
-        mapped.push(await predicate(values[i] as V, keys[i] as K, this as unknown as P))
-      }
-    } else {
-      for (const key of keys) {
-        const value = await this.get(keyspace, storage, key)
-
-        if (value) {
-          mapped.push(await predicate(value as V, key as K, this as unknown as P))
-        }
-      }
-    }
-
-    return mapped
-  }
-  async find<K = string, V = any, P extends CacheProvider = CacheProvider>(
-    keyspace: string, storage: string, predicate: (value: V, key: K, provider: P) => boolean | Promise<boolean>
-  ): Promise<V | undefined> {
-    const keys = await this.keys(keyspace, storage)
-
-    if (!keys.length) return undefined
-
-    if (this.performanceMode === RedisCacheProviderPerformanceMode.Fast) {
-      const values = await this.redisClient.mGet(
-        keys.map(key => this.getRedisKey(keyspace, storage, key))
-      )
-
-      for (let i = 0; i < keys.length; i++) {
-        if (!values[i]) continue
-
-        if (await predicate(values[i] as V, keys[i] as K, this as unknown as P)) {
-          return values[i] as V
-        }
-      }
-    } else {
-      for (const key of keys) {
-        const value = await this.get(keyspace, storage, key)
-
-        if (value && await predicate(value as V, key as K, this as unknown as P)) {
-          return value as V
-        }
-      }
-    }
-
-    return undefined
-  }
-  async count<K = string, V = any, P extends CacheProvider = CacheProvider>(
-    keyspace: string, storage: string, predicate: (value: V, key: K, provider: P) => boolean | Promise<boolean>
-  ): Promise<number> {
-    const keys = await this.keys(keyspace, storage)
-
-    if (!keys.length) return 0
-
-    let count = 0
-
-    if (this.performanceMode === RedisCacheProviderPerformanceMode.Fast) {
-      const values = await this.redisClient.mGet(
-        keys.map(key => this.getRedisKey(keyspace, storage, key))
-      )
-
-      for (let i = 0; i < keys.length; i++) {
-        if (!values[i]) continue
-
-        if (await predicate(values[i] as V, keys[i] as K, this as unknown as P)) {
-          count++
-        }
-      }
-    } else {
-      for (const key of keys) {
-        const value = await this.get(keyspace, storage, key)
-
-        if (value && await predicate(value as V, key as K, this as unknown as P)) {
-          count++
-        }
-      }
-    }
-
-    return count
-  }
-  async counts<K = string, V = any, P extends CacheProvider = CacheProvider>(
-    keyspace: string, storage: string, predicates: ((value: V, key: K, provider: P) => boolean | Promise<boolean>)[]
-  ): Promise<number[]> {
-    const keys = await this.keys(keyspace, storage)
-
-    if (!keys.length) return []
-
-    const counts: number[] = []
-
-    for (const predicate of predicates) {
-      let count = 0
-
-      if (this.performanceMode === RedisCacheProviderPerformanceMode.Fast) {
-        const values = await this.redisClient.mGet(
-          keys.map(key => this.getRedisKey(keyspace, storage, key))
-        )
-
-        for (let i = 0; i < keys.length; i++) {
-          if (!values[i]) continue
-
-          if (await predicate(values[i] as V, keys[i] as K, this as unknown as P)) {
-            count++
-          }
-        }
-      } else {
-        for (const key of keys) {
-          const value = await this.get(keyspace, storage, key)
-
-          if (value && await predicate(value as V, key as K, this as unknown as P)) {
-            count++
-          }
-        }
-      }
-
-      counts.push(count)
-    }
-
-    return counts
   }
   async keys<K = string>(keyspace: string, storage: string): Promise<K[]> {
-    return (await this.redisClient.keys(
-      this.getRedisKey(keyspace, storage, '*')
-    ))
-      .map(key => key.split(':')[2]) as K[]
+    return (await this.redisClient.hkeys(
+      this.getStorageKey(keyspace, storage)
+    )) as K[]
   }
   async values<V = any>(keyspace: string, storage: string): Promise<V[]> {
-    const keys = await this.keys(keyspace, storage)
-    return (await this.redisClient.mGet(keys)) as V[]
+    return (await this.redisClient.hvals(
+      this.getStorageKey(keyspace, storage)
+    )) as V[]
   }
   async entries<K = string, V = any>(keyspace: string, storage: string): Promise<[K, V][]> {
-    const keys = await this.keys(keyspace, storage)
-    const values = (await this.redisClient.mGet(keys)) as V[]
-
-    return keys.map((key, index) => [ key, values[index] ] as [K, V])
+    return Object.entries(await this.redisClient.hgetall(
+      this.getStorageKey(keyspace, storage)
+    )) as [K, V][]
   }
   async init() {
     await this.redisClient.connect()
